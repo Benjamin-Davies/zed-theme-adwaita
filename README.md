5d821e51d1b17eb4e4d25beba24b4802a9ccb42f
Benjamin Davies <bentendavies@gmail.com>
2024-06-11 14:02:12 +1200

    Import theme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Adwaita theme for Zed
+
+Port of https://github.com/piousdeer/vscode-adwaita.
---
+++ import.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ev
+
+theme_importer=${theme_importer:-"../zed/target/debug/theme_importer"}
+vscode_adwaita=${vscode_adwaita:-"../vscode-adwaita"}
+
+mkdir -p themes
+
+for src in $vscode_adwaita/themes/*.json; do
+    basename=$(basename $src)
+    dest=./themes/$basename
+
+    echo "Importing $basename"
+    $theme_importer $src -o $dest
+done
+
+cat themes/adwaita-*.json | jq -n ".themes = [inputs]" > themes/adwaita.json
+
+rm themes/adwaita-*.json
